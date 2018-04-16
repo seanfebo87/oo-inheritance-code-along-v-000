c8883be1f88c76c34744c5964b9134871be60393
Sean Febo <seanfebo@techie.com>
2018-04-16 06:41:14 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,11 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(wheel_number, wheel_size)
+    @wheel_number = wheel_number
+    @wheel_size = wheel_size
+  end
+  
+  def go 
+    "v
 
 end
